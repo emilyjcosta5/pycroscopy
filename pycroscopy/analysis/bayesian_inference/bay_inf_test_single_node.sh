ab6bbeb358dfa73a07bde0a9f00a697c9c61b92a
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-07-23 10:07:14 -0400

    Update bay_inf_test_single_node.sh

--- pycroscopy/analysis/bayesian_inference/bay_inf_test_single_node.sh
+++ pycroscopy/analysis/bayesian_inference/bay_inf_test_single_node.sh
@@ -1,1 +1,3 @@
+export PATH=/gpfs/alpine/gen011/world-shared/native-build/anaconda3/bin:$PATH
+export PYTHONPATH=/gpfs/alpine/gen011/scratch/ecost020/pycroscopy:$PYTHONPATH
 jsrun -n1 -a1 -g6 -c42 python bay_inf_test_single_node.py
