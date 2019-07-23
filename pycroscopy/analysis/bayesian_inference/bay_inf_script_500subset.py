fdb26426fc009578c4e324894d506a90a66a1db8
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-07-23 08:46:29 -0400

    Added comments and descriptions

--- pycroscopy/analysis/bayesian_inference/bay_inf_script_500subset.py
+++ pycroscopy/analysis/bayesian_inference/bay_inf_script_500subset.py
@@ -12,14 +12,20 @@ h5_path = 'pzt_nanocap_6_split_bayesian_compensation_R_correction(EmilyAddedSubs
 #with h5py.File(h5_path, mode='r+', driver='mpio', comm=MPI.COMM_WORLD) as h5_f:
 with h5py.File(h5_path, mode='r+') as h5_f:
     h5_grp = h5_f['Measurement_000/Channel_000']
-
-    h5_resh = h5_f['Measurement_000/Channel_000/Raw_Data-FFT_Filtering_000/Filtered_Data-Reshape_000/Reshaped_Data']
+    #original dataset
+    h5_resh = h5_grp['Raw_Data-FFT_Filtering_000/Filtered_Data-Reshape_000/Reshaped_Data']
+    # Each 'X' axes has 256 images, so [0,1] have 512 images
     myDict = {}
     myDict['X'] = [0,1]
+    # Creates object from original data
     abi = AdaptiveBayesianInference(h5_resh)
+    #slice data
     subset = abi.h5_main.slice_to_dataset(slice_dict=myDict)
+    #create object from sliced data
     abi_subset = AdaptiveBayesianInference(subset)
+    #compute
     h5_bayes_group = abi_subset.compute()
+    #copy result group to new h5 file
     with h5py.File('bayesian_results.h5', 'a') as f:
         f.create_group("results0")
         h5py.copy(h5_bayes_group, f, name="h5_bayes_group")
