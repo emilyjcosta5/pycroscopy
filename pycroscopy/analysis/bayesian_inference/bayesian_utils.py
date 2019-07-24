86b51b0300ccbf2a1c49657183e588d67ac3ec83
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-07-24 10:29:15 -0400

    Update bayesian_utils.py

--- pycroscopy/analysis/bayesian_inference/bayesian_utils.py
+++ pycroscopy/analysis/bayesian_inference/bayesian_utils.py
@@ -217,7 +217,7 @@ def _run_bayesian_inference(V, i_meas, M, dx, x, f, V0, Ns, dvdt, verbose=False)
     breakpoint()
     dV = np.divide(dV,dt)
     breakpoint()
-    dV = np.concatenate((dV, np.array([dV[-1]])))
+    dV = np.concatenate((dV, numpy.asarray([dV[-1]])))
     N = V.size
     #dx = 2*V0/(M-2)
     #x = np.arange(-V0, V0+dx, dx)[np.newaxis].T
