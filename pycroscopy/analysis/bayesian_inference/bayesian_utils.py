d176793a37c3a9d5a3c9152e527961983a2a134e
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-07-24 16:44:52 -0400

    Update bayesian_utils.py

--- pycroscopy/analysis/bayesian_inference/bayesian_utils.py
+++ pycroscopy/analysis/bayesian_inference/bayesian_utils.py
@@ -239,17 +239,14 @@ def _run_bayesian_inference(V, i_meas, M, dx, x, f, V0, Ns, dvdt, verbose=False)
         # Note: ix will be used to index into arrays, so it is one less
         # than the ix used in the Matlab code
         ix = math.floor((V[j] + V0)/dx) + 1
-        #print(A.shape)
-        #print(j)
-        #print(x.shape)
         ix = min(ix, x.size - 1)
         ix = max(ix, 1)
-        #print(ix)
         A[j, ix] = int(np.divide(np.subtract(V[j],x[ix-1]),np.subtract(x[ix],x[ix-1])))
-        A[j, ix-1] = int(np.subtract(1, np.divide(np.subtract(V[j],x[ix-1]),np.subtract(x[ix],x[ix-1]))))
+        A[j, ix-1] = int(np.subtract(1, np.divide(np.subtract(V[j],x[ix-1]),
+                                                  np.subtract(x[ix],x[ix-1]))))
         #A[j, ix-1] = (1 - (V[j] - x[ix-1])/(x[ix] - x[ix-1]));
     print(type(np.transpose(np.add(dV,ff*r_extra*V))))
-    print(type(A))
+    print(type(M))
     A[:, M] = np.asarray(np.transpose(np.add(dV,ff*r_extra*V)))
     breakpoint()
     
@@ -262,8 +259,9 @@ def _run_bayesian_inference(V, i_meas, M, dx, x, f, V0, Ns, dvdt, verbose=False)
         ix = min(ix, x.size - 1)
         ix = max(ix, 1)
         breakpoint()
-        A1[j, ix] = V[j]*(V[j] - x[ix-1])/(x[ix] - x[ix-1])
-        A1[j, ix-1] = V[j]*(1 - (V[j] - x[ix-1])/(x[ix] - x[ix-1]))
+        A[j, ix] = int(np.divide(np.subtract(V[j],x[ix-1]),np.subtract(x[ix],x[ix-1])))
+        A[j, ix-1] = int(np.subtract(1, np.divide(np.subtract(V[j],x[ix-1]),
+                                                  np.subtract(x[ix],x[ix-1]))))
     A1[:, M] = (dV + ff*r_extra*V).T # transpose again here
 
     # A rough guess for the initial condition is a bunch of math stuff
