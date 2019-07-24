b5c47fa2437739b8eab463b9942f9f0074d8e8aa
Emily Costa <36118116+emilyjcosta5@users.noreply.github.com>
2019-07-24 16:22:26 -0400

    Update bayesian_utils.py

--- pycroscopy/analysis/bayesian_inference/bayesian_utils.py
+++ pycroscopy/analysis/bayesian_inference/bayesian_utils.py
@@ -247,8 +247,9 @@ def _run_bayesian_inference(V, i_meas, M, dx, x, f, V0, Ns, dvdt, verbose=False)
         A[j, ix] = int(np.divide(np.subtract(V[j],x[ix-1]),np.subtract(x[ix],x[ix-1])))
         A[j, ix-1] = int(np.subtract(1, np.divide(np.subtract(V[j],x[ix-1]),np.subtract(x[ix],x[ix-1]))))
         #A[j, ix-1] = (1 - (V[j] - x[ix-1])/(x[ix] - x[ix-1]));
-    print((np.add(dV,ff*r_extra*V)).shape)
-    A[:, M] = np.asarray(np.add(dV,ff*r_extra*V))
+    print((np.transpose(np.add(dV,ff*r_extra*V))).shape)
+    print(A.shape)
+    A[:, M] = np.asarray(np.transpose(np.add(dV,ff*r_extra*V)))
     breakpoint()
     
     # Similar to above, but used to simulate data and invert for E(s|y)
