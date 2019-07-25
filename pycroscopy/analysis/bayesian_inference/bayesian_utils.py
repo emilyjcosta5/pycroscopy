d53c4bc0539b632c48c82448508e9d6ac9387e85
emilyjcosta5 <Pa$$word5>
2019-07-25 10:43:35 -0400

    updates for GPU-ify

--- pycroscopy/analysis/bayesian_inference/bayesian_utils.py
+++ pycroscopy/analysis/bayesian_inference/bayesian_utils.py
@@ -302,7 +302,7 @@ def _run_bayesian_inference(V, i_meas, M, dx, x, f, V0, Ns, dvdt, verbose=False)
     S1 = cp.zeros((M+2, 1))
     print(mr.shape)
     print(type(mr))
-    print(cp.expand_dims(cp.expand_dims(cp.asarray(r_extra), axis=0), axis=1).shape)
+    print(cp.expand_dims(cp.expand_dims(cp.asarray(r_extra), axis=0), axis=1))
     mm = cp.concatenate((mr, cp.expand_dims
         (cp.expand_dims(cp.asarray(r_extra), axis=0).shape, axis=1)))
     print(mm.shape)
